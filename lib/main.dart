edd8d5af6b10d22d39f615bcadffb0604382e2a8
noobie7 <mahendra060704@gmail.com>
2021-09-18 19:37:43 +0530

    Corrected the widget tree and reFormatted the code

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home: Home(),
-));
+      home: Home(),
+    ));
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -18,146 +18,120 @@ class _HomeState extends State<Home> {
     return Scaffold(
       backgroundColor: Colors.indigoAccent,
       appBar: AppBar(
-        title : Text(
+          title: Text(
             'Ninja ID Card',
-          style: TextStyle(
-            color : Colors.indigoAccent,
-            fontSize: 25,
-            fontFamily: 'MajorMono'
+            style: TextStyle(
+                color: Colors.indigoAccent,
+                fontSize: 25,
+                fontFamily: 'MajorMono'),
           ),
-        ),
-        centerTitle: true,
-        backgroundColor: Colors.amberAccent,
-        elevation: 0
-
-      ),
+          centerTitle: true,
+          backgroundColor: Colors.amberAccent,
+          elevation: 0),
       floatingActionButton: FloatingActionButton(
-        onPressed: (){
+        onPressed: () {
           setState(() {
             _ninjaLevel++;
           });
         },
         backgroundColor: Colors.amberAccent,
-        child: Icon(
-          Icons.add,
-          color : Colors.indigo,
-          size : 30
-        ),
+        child: Icon(Icons.add, color: Colors.indigo, size: 30),
       ),
       body: Center(
-        child: Padding(
-          padding: EdgeInsets.all(5.0),
-          child: Expanded(
-                child: Padding(
-                  padding: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-                  child: Row(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-
-                      children:[
-                        Expanded(
-
-                           child: Column(
-                                mainAxisAlignment: MainAxisAlignment.start,
-                                crossAxisAlignment: CrossAxisAlignment.start,
-
-                                children: [
-                                  SizedBox(
-                                    height: 50,
-                                  ),
-                                  Text(
-                                      'NAME',
-                                      style: TextStyle(
-                                          color: Colors.amber,
-                                          fontFamily: 'Cutive',
-                                          letterSpacing: 2,
-                                          fontWeight: FontWeight.bold,
-                                          fontSize: 24
-                                      )
-                                  ),
-                                  SizedBox(
-                                    height: 5,
-                                  ),
-                                  Text(
-                                      'Chun-Li',
-                                      style: TextStyle(
-                                          color: Colors.amberAccent[100],
-                                          fontFamily: 'MajorMono',
-                                          letterSpacing: 2,
-                                          fontSize: 30
-                                      )
-                                  ),
-                                  SizedBox(
-                                    height: 50,
-                                  ),
-                                  Text(
-                                      'Current Ninja Level',
-                                      style : TextStyle(
-                                          color : Colors.amber,
-                                          fontFamily: 'Cutive',
-                                          fontSize: 24
-                                      )
-                                  ),
-                                  SizedBox(
-                                    height: 10,
-                                  ),
-                                  Text(
-                                    '$_ninjaLevel',
-                                    style: TextStyle(
-                                        color: Colors.amberAccent[100],
-                                        fontFamily: 'MajorMono',
-                                        letterSpacing: 2,
-                                        fontSize: 30
-                                    ),
-                                  ),
-                                  SizedBox(
-                                    height: 50,
-                                  ),
-                                  Row(
-                                      children: [
-                                        Icon(
-                                          Icons.email_outlined,
-                                          color: Colors.amber,
-                                          size: 20,
-                                        ),
-                                        SizedBox(
-                                            width: 10
-                                        ),
-                                        Text(
-                                          'ninja69@lmao.com',
-                                          style: TextStyle(
-                                              color: Colors.amberAccent[100],
-                                              fontFamily: 'MajorMono',
-                                              letterSpacing: 2,
-                                              fontSize: 12
-                                          ),
-                                        )
-                                      ]
-                                  )
-                                ]
-                              )
-                            ),
-                            VerticalDivider(
-                              color: Colors.grey[100],
-                              endIndent: 465,
-                              indent: 35,
-                            ),
-                            SizedBox(
-                              width: 10,
-                            ),
-                            Padding(
-                              padding: EdgeInsets.fromLTRB(5, 30, 10, 30),
-                              child: CircleAvatar(
-                                backgroundImage: AssetImage('assets/index.jpg'),
-                                radius: 60,
-                              ),
-                            )
-                        ],
+        child: Flex(direction: Axis.horizontal, children: [
+          Expanded(
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                    padding: EdgeInsets.all(10.0)
+                ),
+                Expanded(
+                    child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                      SizedBox(
+                        height: 50,
                       ),
+                      Text('NAME',
+                          style: TextStyle(
+                              color: Colors.amber,
+                              fontFamily: 'Cutive',
+                              letterSpacing: 2,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 24)),
+                      SizedBox(
+                        height: 5,
+                      ),
+                      Text('Chun-Li',
+                          style: TextStyle(
+                              color: Colors.amberAccent[100],
+                              fontFamily: 'MajorMono',
+                              letterSpacing: 2,
+                              fontSize: 30)),
+                      SizedBox(
+                        height: 50,
+                      ),
+                      Text('Current Ninja Level',
+                          style: TextStyle(
+                              color: Colors.amber,
+                              fontFamily: 'Cutive',
+                              fontSize: 24)),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Text(
+                        '$_ninjaLevel',
+                        style: TextStyle(
+                            color: Colors.amberAccent[100],
+                            fontFamily: 'MajorMono',
+                            letterSpacing: 2,
+                            fontSize: 30),
+                      ),
+                      SizedBox(
+                        height: 50,
+                      ),
+                      Row(children: [
+                        Icon(
+                          Icons.email_outlined,
+                          color: Colors.amber,
+                          size: 20,
+                        ),
+                        SizedBox(width: 10),
+                        Text(
+                          'ninja69@lmao.com',
+                          style: TextStyle(
+                              color: Colors.amberAccent[100],
+                              fontFamily: 'MajorMono',
+                              letterSpacing: 2,
+                              fontSize: 12),
+                        )
+                      ])
+                    ])),
+                VerticalDivider(
+                  color: Colors.grey[100],
+                  endIndent: 465,
+                  indent: 35,
                 ),
-                  )
+                SizedBox(
+                  width: 10,
                 ),
-              ),
-
+                Padding(
+                  padding: EdgeInsets.fromLTRB(5, 30, 10, 30),
+                  child: CircleAvatar(
+                    backgroundImage: AssetImage('assets/index.jpg'),
+                    radius: 60,
+                  ),
+                )
+              ],
+            ),
+          ),
+          Padding(
+              padding: EdgeInsets.all(10.0)
+          )
+        ]),
+      ),
     );
   }
 }
